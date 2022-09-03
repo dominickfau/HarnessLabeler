26c6b240cf64e37972b13e583a0420d5b00daa81
Dominick Faurote <1hackaweek@gmail.com>
2022-09-02 20:45:49 -0400

    Removed unused code.

--- harnesslabeler/enums.py
+++ harnesslabeler/enums.py
@@ -6,35 +6,7 @@ class PythonEnum(Enum):
         return [e.value for e in cls]
 
 
-class Airworthyness(PythonEnum):
-    Airworthy = "Airworthy"
-    Maintenance = "Under Maintenance"
-    Retired = "Retired"
-
-
-class EquipmentGroup(PythonEnum):
-    Ground = "Ground"
-    """Used for equipment that will stay on the ground."""
-    Airborne = "Airborne"
-    """Used for equipment that will attach to a drone."""
-
-
 class LoginEventType(PythonEnum):
     """Represents a user login log event type."""
     Login = "Login"
     Logout = "Logout"
-
-
-class ThrustDirection(PythonEnum):
-    """Represents a thrust direction."""
-    Vertical = "Vertical"
-    Horizontal = "Horizontal"
-
-
-class UomType(PythonEnum):
-    Count = "Count"
-    Weight = "Weight"
-    Length = "Length"
-    Area = "Area"
-    Volume = "Volume"
-    Time = "Time"
